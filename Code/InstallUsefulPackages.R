144ff498a70342d365f1d20e71060c01cba9d404
insilentio <insilentio@protonmail.ch>
2022-01-05 14:25:37 +0100

    initial commit package installation

---
+++ Code/InstallUsefulPackages.R
@@ -0,0 +1,24 @@
+#some of the most ubiquitous packages
+#together with dependencies, installs most of
+#packages needed in my codes
+install.packages("tidyverse")
+install.packages("caret")
+install.packages("keras")
+install.packages("e1071")
+install.packages("doParallel")
+install.packages("knitr")
+install.packages("xtable")
+install.packages("AppliedPredictiveModeling")
+install.packages("mlbench")
+install.packages("fastICA")
+install.packages("randomForest")
+install.packages("gbm")
+install.packages("gam")
+install.packages("Hmisc")
+install.packages("pROC")
+install.packages("exiftoolr")
+install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
+install.packages("RSQLite")
+install.packages("nnet")
+install.packages("tm")
+
